e2464d4a0bebc6dbd0697907df1236f7ee5bbf8d
Mohammad Usama <99090844+epiicuree@users.noreply.github.com>
2022-10-23 00:30:38 +0500

    Add files via upload

---
+++ Resources/Stack&Queue/linkedlist_stack.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+
+// structure Node definition
+struct Node {
+   int data;
+   struct Node *next;
+};
+struct Node* top = NULL;
+
+
+
+// push function 
+void push(int val) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = val;
+   newnode->next = top;
+   top = newnode;
+}
+
+// pop function
+void pop() {
+   if(top==NULL)
+   cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< top->data <<endl;
+      top = top->next;
+   }
+}
+
+
+// display for printing Stack element
+void display() {
+   struct Node* ptr;
+   if(top==NULL)
+   cout<<"stack is empty";
+   else {
+      ptr = top;
+      cout<<"Stack elements are: ";
+      while (ptr != NULL) {
+         cout<< ptr->data <<" ";
+         ptr = ptr->next;
+      }
+   }
+   cout<<endl;
+}
+
+
+// Driven Program
+int main() {
+   int ch, val;
+   cout<<"1) Push in stack"<<endl;
+   cout<<"2) Pop from stack"<<endl;
+   cout<<"3) Display stack"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1: {
+            cout<<"Enter value to be pushed:"<<endl;
+            cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=4);
+   return 0;
+}
